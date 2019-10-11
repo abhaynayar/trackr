6c97da97761bd68e950fb00b80bd72aaecc5746c
abhaynayar <abhaynayar@gmail.com>
2019-10-11 20:54:11 +0530

    setting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionCreateComponent } from './transactions/transaction-create/transaction-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionCreateComponent
   ],
   imports: [
     BrowserModule,
